c88b9871315db0ac20d4fdd449a31a5030fda345
Guido Salazar <guidoesalazar@gmail.com>
2024-06-12 16:34:22 -0500

    Subiendo pagina

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const username = "GAOV13"; //
+    const username = "GAOV13";
     const reposList = document.getElementById("repos-list");
 
-    fetch(`https://api.github.com/users/${username}/repos`)
+    fetch('https://api.github.com/users/${username}/repos')
         .then(response => response.json())
         .then(data => {
             data.forEach(repo => {

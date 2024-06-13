58a7196fc5226202fb68856b200e60a881104bdc
Guido Salazar <guidoesalazar@gmail.com>
2024-06-12 22:02:12 -0500

    Front end mentor

--- scripts.js
+++ scripts.js
@@ -1,23 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const username = "GAOV13"; // Cambia esto a tu nombre de usuario de GitHub
-    const reposList = document.getElementById("repos-list");
-    
-    // Palabras clave para filtrar los repositorios
-    const keywords = ["mentor", "uva"]; // Ajusta estas palabras clave según tus necesidades
+function choose_link(url, val){
+    var info = document.getElementsByClassName("info");
+    for(i = 0; i < info.length; ++i){
+        info[i].className = info[i].className.replace(" active", "");
+    }
 
-    fetch(`https://api.github.com/users/${username}/repos`)
-        .then(response => response.json())
-        .then(data => {
-            // Filtrar los repositorios basados en las palabras clave
-            const filteredRepos = data.filter(repo => {
-                return keywords.some(keyword => repo.name.toLowerCase().includes(keyword.toLowerCase()));
-            });
-
-            filteredRepos.forEach(repo => {
-                const listItem = document.createElement("li");
-                listItem.textContent = repo.name;
-                reposList.appendChild(listItem);
-            });
-        })
-        .catch(error => console.error('Error fetching repos:', error));
-});
+    info[val].className = info[val].className.replace(" inactive", "");
+    info[val].className += " active";
+    link = url;
+    var iframe = document.getElementById("observar");
+    iframe.src = url;
+}

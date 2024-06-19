314053c3c89ba925ea26d371838e20605ab694aa
Guido Salazar <guidoesalazar@gmail.com>
2024-06-18 19:59:31 -0500

    Subiendo cambios

--- scripts/fron_end_scripts.js
+++ scripts/fron_end_scripts.js
@@ -1,20 +1,85 @@
 data_front_end = {
     "NEWBIE": ["base-apparel-coming-soon-master", "interactive-rating-component-main", "nft-preview-card-component-main",
                "order-summary-component-main", "product-preview-card-component-main", "qr-code-component-main",
-               "results-summary-component-main", "social-links-profile-main"
+               "results-summary-component-main", "social-links-profile-main", "blog-preview-card-main"
               ],
     "Junior": ["newsletter-sign-up-with-success-message-main", "interactive-card-details-form-main"],
+    "Intermediate": [],
+    "Advance": [],
+    "Guru": [],
 };
 
-const frontend_challenges = document.getElementById("frontend-challenge")
+const frontend_challenges = document.getElementById("frontend-challenge");
+const pagination = document.getElementById('pagination');
+let key = "";
 
-/*
-Actualizar para poner limites de proyectos que se visualizan en la pagina front
-end.
+let currentPage = 1;
+const cardsPerPage = 5
 
-// let currentPage = 1;
-// const cardsPerPage = 5;
-*/
+function updateView() {
+    renderCards(currentPage);
+    renderPagination();
+}
+
+function renderCards(page) {
+    frontend_challenges.innerHTML = "";
+    var path= './FrontEnd-Mentor/' + key + "/";
+    var data = data_front_end[key]
+    var value = 0;
+    const start = (page - 1) * cardsPerPage;
+    const end = start + cardsPerPage;
+    const cardsToRender = data.slice(start, end);
+    console.log(data)
+    console.log(cardsToRender)
+    cardsToRender.forEach(folder => {
+        var path_to_search = path + folder + "/index.html'";
+        var name = folder.replace(/[-#$]/g, ' ')
+        const liElement = document.createElement('ul');
+        liElement.innerHTML = `<li class="info" onclick="choose_link('${path_to_search},${value})">${name}</li>`;
+        frontend_challenges.appendChild(liElement);
+        value +=1 ;
+    });
+}
+
+function renderPagination(){
+    pagination.innerHTML = '';
+    const totalPages = Math.ceil(data_front_end[key].length / cardsPerPage);
+
+    console.log(totalPages)
+
+    if(totalPages <= 1) return;
+ 
+    const createPageButton = (page) => {
+        const button = document.createElement('button');
+        button.innerText = page;
+        button.classList.add("t-button");
+        button.addEventListener('click', () => {
+            currentPage = page;
+            updateView();
+        });
+        return button;
+    };
+
+    pagination.appendChild(createPageButton(1));
+
+    if (currentPage > 3) {
+        const dots = document.createElement('span');
+        dots.innerText = '...';
+        pagination.appendChild(dots);
+    }
+
+    for (let i = Math.max(2, currentPage - 1); i <= Math.min(totalPages - 1, currentPage + 1); i++) {
+        pagination.appendChild(createPageButton(i));
+    }
+
+    if (currentPage < totalPages - 2) {
+        const dots = document.createElement('span');
+        dots.innerText = '...';
+        pagination.appendChild(dots);
+    }
+
+    pagination.appendChild(createPageButton(totalPages));
+}
 
 function choose_link(url, val){
     set_active(val, "info");
@@ -25,25 +90,50 @@ function choose_link(url, val){
 
 function choose_elemets(val, text){
     set_active(val, "t-info");
-    frontend_challenges.innerHTML = "";
-    var path= './FrontEnd-Mentor/' + text + "/";
-    var data = data_front_end[text]
-    var value = 0;
-    data.forEach(folder => {
-        var path_to_search = path + folder + "/index.html'";
-        var name = folder.replace(/[-#$]/g, ' ')
-        const liElement = document.createElement('ul');
-        liElement.innerHTML = `<li class="info" onclick="choose_link('${path_to_search},${value})">${name}</li>`;
-        frontend_challenges.appendChild(liElement);
-        value +=1 ;
-    });
+    key = text;
+    currentPage = 1;
+    updateView()
 }
 
 function set_active(val, text){
     var info = document.getElementsByClassName(text);
+    console.log(info)
     for(i = 0; i < info.length; ++i){
         info[i].className = info[i].className.replace(" active", "");
     }
 
     info[val].className += " active";
 }
+
+// function choose_link(url, val){
+//     set_active(val, "info");
+//     link = url;
+//     var iframe = document.getElementById("observar");
+//     iframe.src = url;
+// }
+
+// function choose_elemets(val, text){
+//     set_active(val, "t-info");
+//     frontend_challenges.innerHTML = "";
+//     var path= './FrontEnd-Mentor/' + text + "/";
+//     var data = data_front_end[text]
+//     var value = 0;
+//     data.forEach(folder => {
+//         var path_to_search = path + folder + "/index.html'";
+//         var name = folder.replace(/[-#$]/g, ' ')
+//         const liElement = document.createElement('ul');
+//         liElement.innerHTML = `<li class="info" onclick="choose_link('${path_to_search},${value})">${name}</li>`;
+//         frontend_challenges.appendChild(liElement);
+//         value +=1 ;
+//     });
+// }
+
+// function set_active(val, text){
+//     var info = document.getElementsByClassName(text);
+//     console.log(info)
+//     for(i = 0; i < info.length; ++i){
+//         info[i].className = info[i].className.replace(" active", "");
+//     }
+
+//     info[val].className += " active";
+// }
---
+++ styles.md
@@ -0,0 +1,17 @@
+# Fron-end Style
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Background
+
+- Grey: rgb(238, 238, 238)
+- footer, header, buttons: rgb(55, 58, 64)
+- hover: rgb(104, 109, 118)
+- hover text: rgb(220, 95, 0)
